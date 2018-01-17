f92fe6f17771649458e9c274630ca417ceb828bc
Arthur Puthin <arthur.rprp@gmail.com>
2018-01-17 00:22:36 -0200

    feat: adds vendor chunking and auto-html insertion of scripts

--- upstarmusic-app/webpack.config.js
+++ upstarmusic-app/webpack.config.js
@@ -1,11 +1,22 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'react-redux', 'react-dom', 'react-router', 'react-input-range',
+  'redux', 'redux-form', 'redux-thunk', 'faker', 'lodash'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
+    // name pairs up with the above entries
+    // chunkhash helps the browser keeps track of what files changed
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -19,5 +30,15 @@ module.exports = {
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    // removes the duplicate (commonly used) dependencies
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    // adds the bundle script tags into the html file
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
